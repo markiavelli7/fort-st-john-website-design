a9d16c199376460d330b08e70d5a0ccbd1cd6f3a
Mark Jensen <office@jensendesignshop.com>
2021-05-04 15:02:03 -0700

    update sitemap

--- src/components/recentWork/RecentWork.js
+++ src/components/recentWork/RecentWork.js
@@ -7,34 +7,23 @@ import RecentWorkFeatured from "./RecentWorkFeatured";
 import FourMile from "../../../public/svgs/FourMile";
 import PonderosaSvg from "../../../public/svgs/Ponderosa";
 import I2Svg from "../../../public/svgs/I2";
-import FurnacePro from "../../../public/svgs/FurnacePro";
 
 export default function RecentWork() {
   return (
     <div sx={styles.recentWork}>
       <RecentWorkFSFeatured
         style={styles.fullscreenFourMile}
-        bodyOne="FSJ Furnace Pro - Furnace sales, repairs and installation in Fort St John."
-        bodyTwo="A streamlined website that gets right to point, right away."
-        lqipImgSrc={require("public/images/home/fsj-furnace-pro.jpeg?lqip")}
-        imgSrc="/images/home/fsj-furnace-pro.jpeg"
-        imgAlt="FSJ Furnace Pro, website built by Jensen Design Shop."
-        featuredSVG={<FurnacePro />}
-        linkText="Visit FSJ Furnace Pro"
-        linkToWebsite="https://fsjfurnacepro.com"
+        bodyOne="4 Mile Tree Farm - the spruce &amp; blue spruce tree farm for Fort St John and the surrounding area."
+        bodyTwo="A simple and easy-to-use website designed for the purpose of driving increased tree sales."
+        lqipImgSrc={require("public/images/home/four-mile-tree-farm-fsj.jpeg?lqip")}
+        imgSrc="/images/home/four-mile-tree-farm-fsj.jpeg"
+        imgAlt="Four Mile Tree Farm, website built by Jensen Design Shop."
+        featuredSVG={<FourMile />}
+        linkText="Visit 4 Mile Tree Farm"
+        linkToWebsite="https://www.fourmiletreefarm.com"
       />
       <RecentWorkFeatured
         style={styles.mobileFeaturedOne}
-        bodyOne="FSJ Furnace Pro - Furnace sales, repairs and installation in Fort St John."
-        bodyTwo="A streamlined website that gets right to point, right away."
-        lqipImgSrc={require("public/images/home/fsj-furnace-pro.jpeg?lqip")}
-        imgSrc="/images/home/fsj-furnace-pro.jpeg"
-        imgAlt="FSJ Furnace Pro, website built by Jensen Design Shop."
-        featuredSVG={<FurnacePro />}
-        linkText="Visit FSJ Furnace Pro"
-        linkToWebsite="https://fsjfurnacepro.com"
-      />
-      <RecentWorkFeatured
         bodyOne="4 Mile Tree Farm - the spruce/blue spruce tree farm for Fort St John and the surrounding area."
         bodyTwo="A simple and easy-to-use website designed for the purpose of driving increased tree sales."
         lqipImgSrc={require("public/images/home/four-mile-tree-farm-fsj.jpeg?lqip")}
@@ -54,7 +43,7 @@ export default function RecentWork() {
         linkText="Visit Ponderosa Ventures"
         linkToWebsite="https://ponderosa-ventures.now.sh/"
       />
-      {/* <RecentWorkFeatured
+      <RecentWorkFeatured
         bodyOne="i2 Optimization - streamlining midstream operations for companies across North America."
         bodyTwo="A straight to the point site where oil and gas operations management can go to get answers."
         lqipImgSrc={require("public/images/home/i2-optimization-fsj.jpeg?lqip")}
@@ -63,7 +52,7 @@ export default function RecentWork() {
         featuredSVG={<I2Svg />}
         linkText="Visit i2 Optimization"
         linkToWebsite="https://www.i2optimization.com"
-      /> */}
+      />
     </div>
   );
 }
